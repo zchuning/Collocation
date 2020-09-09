normalization and other fixes

dreamer/dreamer.py
@@ -104,9 +104,11 @@ class Dreamer(tools.Module):
     # Create variables for checkpoint
     self._metrics['expl_amount']
     if not self._c.metrics_fix:
+      self._metrics['opt_rewards']
       self._metrics['opt_dynamics']
       self._metrics['opt_action_violation']
-      self._metrics['opt_rewards']
+      self._metrics['opt_dynamics_coeff']
+      self._metrics['opt_action_coeff']
     self._float = prec.global_policy().compute_dtype
     self._dataset = iter(load_dataset(datadir, self._c))
     self._build_model()
@@ -333,7 +335,7 @@ class Dreamer(tools.Module):
     with (self._c.logdir / 'metrics.jsonl').open('a') as f:
       f.write(json.dumps({'step': step, **dict(metrics)}) + '\n')
     [tf.summary.scalar('agent/' + k, m) for k, m in metrics]
-    print(f'[{step}]', ' / '.join(f'{k} {v:.1f}' for k, v in metrics))
+    print(f'[{step}]: {self._c.logdir} , ', ' / '.join(f'{k} {v:.1f}' for k, v in metrics))
     self._writer.flush()
 
 

dreamer/dreamer_colloc.py
@@ -53,6 +53,7 @@ def define_config():
   config.lambda_lr = 1
   config.lam_step = 10
   config.dyn_threshold = 1e-1
+  config.coeff_normalization = 10000
   config.nu_lr = 100
   config.dyn_loss_scale = 5000
   config.act_loss_scale = 100
@@ -124,7 +125,7 @@ class DreamerColloc(Dreamer):
     dyn_c = tf.sqrt(lam)[:, None] * self._c.dyn_res_wt
     act_c = tf.sqrt(nu) * act_res
     rew_c = tf.cast(self._c.rew_res_wt, act_c.dtype)
-    normalize = 1000 / (tf.reduce_sum(dyn_c) + tf.reduce_sum(act_c) + tf.reduce_sum(rew_c))
+    normalize = self._c.coeff_normalization / (tf.reduce_mean(dyn_c) + tf.reduce_mean(act_c) + tf.reduce_mean(rew_c))
     dyn_c = dyn_c * normalize
     act_c = act_c * normalize
     rew_c = rew_c * normalize
@@ -136,7 +137,14 @@ class DreamerColloc(Dreamer):
     # rew_res = self._c.rew_res_wt * tf.sqrt(-tf.clip_by_value(rew-100000, -np.inf, 0))[:, None] # shifted reward
     objective = tf.concat([dyn_res, act_res, rew_res], 1)
     return objective
-
+  
+  @tf.function
+  def opt_step(self, plan, init_feat, goal_feat, lam, nu):
+    init_residual_func = lambda x: (x[:, :-self._actdim] - init_feat) * 1000
+    pair_residual_func = lambda x_a, x_b : self.pair_residual_func_body(x_a, x_b, goal_feat, lam, nu)
+    plan = gn_solver.solve_step_inference(pair_residual_func, init_residual_func, plan, damping=self._c.gn_damping)
+    return plan
+  
   def collocation_so(self, obs, goal_obs, save_images, step, init_feat=None, verbose=True):
     hor = self._c.planning_horizon
     feat_size = self._c.stoch_size + self._c.deter_size
@@ -157,14 +165,7 @@ class DreamerColloc(Dreamer):
     plan = tf.reshape(plan, [1, hor + 1, var_len_step])
     lam = tf.ones(hor)
     nu = tf.ones([hor, self._actdim])
-
-    @tf.function
-    def opt_step(plan, init_feat, goal_feat, lam, nu):
-      init_residual_func = lambda x: (x[:, :-self._actdim] - init_feat) * 1000
-      pair_residual_func = lambda x_a, x_b : self.pair_residual_func_body(x_a, x_b, goal_feat, lam, nu)
-      plan = gn_solver.solve_step_inference(pair_residual_func, init_residual_func, plan, damping=damping)
-      return plan
-
+    
     # Run second-order solver
     dyn_losses, act_losses, rewards = [], [], []
     dyn_coeffs, act_coeffs = [], []
@@ -199,7 +200,7 @@ class DreamerColloc(Dreamer):
           lam = lam * self._c.lam_step
         if dyn_loss < dyn_threshold / 10:
           lam = lam / self._c.lam_step
-        if act_loss > act_threshold:
+        if act_loss / hor > act_threshold:
           nu = nu * self._c.lam_step
         # lam += self._c.lambda_lr * dyn_res_sq
         # nu += self._c.nu_lr * act_res_sq

dreamer/dreamer_colloc_online.py
@@ -103,6 +103,7 @@ class DreamerCollocOnline(dreamer_colloc.DreamerColloc):
       if tf.equal(log_images, True):
         self._image_summaries(data, embed, image_pred)
 
+  @tf.function
   def _policy_summaries(self, feat_pred, act_pred, init_feat):
     # Collocation
     img_pred = self._decode(feat_pred).mode()
@@ -119,7 +120,7 @@ class DreamerCollocOnline(dreamer_colloc.DreamerColloc):
     img_pred = self._decode(tf.concat((init_feat[None], feat_pred), 1)).mode()
     tools.graph_summary(self._writer, tools.video_summary, 'model_mean', img_pred + 0.5)
 
-  @tf.function
+  # @tf.function
   def plan(self, feat, log_images):
     # TODO speed this up
     # Note: it is possible to get rid of tf.function by removing lambda-functions in collocation_so. This is possible