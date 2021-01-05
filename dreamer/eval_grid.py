d9fac0f6364220712c2d91d2931fb852d473a263
Chuning (Tony) Zhu <chuning.zhu@gmail.com>
2021-01-06 01:26:19 +0800

    Fix distance calculation in eval_grid.py

--- dreamer/eval_grid.py
+++ dreamer/eval_grid.py
@@ -13,11 +13,13 @@ PM_OBSTACLE_MAX_GOAL_DIST = 7.0 # 3 + 1.5 + 2.5
 
 def assign_pm_obstacle(init_state, res):
     # Assign to bin based on Manhattan distance
+    goal_x, goal_y = 0.5, 1.5
+    wall_y_min = -1
     x, y = init_state[:2]
     if x <= 0:
-        goal_dist = - x + 0.5 + 2 * abs(y + 1)
+        goal_dist = (goal_x - x) + abs(y - wall_y_min) + (goal_y - wall_y_min)
     else:
-        goal_dist = abs(x - 0.5) + abs(y - 1.5)
+        goal_dist = abs(goal_x - x) + abs(y - goal_y)
     interval = PM_OBSTACLE_MAX_GOAL_DIST / res
     return int(goal_dist % interval)
 
@@ -25,6 +27,7 @@ def assign_pm_obstacle(init_state, res):
 def assign_mw_push(init_state, res):
     # Returns an integer index
     pass
+
 
 def generate_grid(filenames, assign_fn, res):
     # Returns array of length <res>
@@ -39,10 +42,11 @@ def generate_grid(filenames, assign_fn, res):
         ind = assign_fn(init_state, res)
         rews[ind] += episode['reward'].sum()
         frqs[ind] += 1
-
+    # Avoid division by zero
     frqs[frqs == 0] = 1.0
     rews = rews / frqs
     return rews, frqs
+
 
 def plot_grid(rews, frqs, title, figdir):
     plt.plot(rews)
@@ -51,6 +55,7 @@ def plot_grid(rews, frqs, title, figdir):
     plt.xlabel('Task difficulty')
     plt.savefig(figdir)
 
+
 def eval_grid(config):
     filenames = sorted(glob(f'{config.logdir}/episodes/*.npz'))
     # Take the most recent episodes
@@ -58,11 +63,13 @@ def eval_grid(config):
     task = config.task
     res = config.resolution
     if 'pm_obstacle_long' in task:
-        rews, frqs = generate_grid(filenames, assign_pm_obstacle, res)
+        assign_fn = assign_pm_obstacle
     elif 'SawyerPushEnv' in task:
-        rews, frqs = generate_grid(filenames, assign_mw_push, res)
+        assign_fn = assign_mw_push
     else:
         raise NotImplementedError(task)
+
+    rews, frqs = generate_grid(filenames, assign_fn, res)
     plot_grid(rews, frqs, f'[{config.planner}] {task}', config.figdir)
 
 
